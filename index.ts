415494988533f209e7ba59ba39efde4c617b0b89
Arthur Andrade <arthurfelandrade@gmail.com>
2023-03-06 19:00:31 -0300

    Add GraphQL inital config
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- index.ts
+++ index.ts
@@ -1,5 +1,46 @@
-function main(message: string) {
-	console.log(message);
-}
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-main("Hello World");
+const books = [
+	// Mock de dados
+	{
+		title: "The Awakening",
+		author: "Kate Chopin",
+	},
+	{
+		title: "City of Glass",
+		author: "Paul Auster",
+	},
+];
+
+// Tipagem do GraphQL
+const typeDefs = `#graphql
+	type Book {
+		title: String
+		author: String
+	}
+
+	type Query { #Especial type Query
+		books: [Book]
+	}
+`;
+
+const resolvers = {
+	// Resolver
+	Query: {
+		books: () => books,
+	},
+};
+
+//Inicialização do servidor
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+// Inicio da aplicação
+startStandaloneServer(server, {
+	listen: { port: 4000 },
+}).then(({ url }) => {
+	console.log(`🚀  Server ready at: ${url}`);
+});
